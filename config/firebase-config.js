c40006a00d576b0bc72d203d21992317f1d244f6
Valdir Capodeferro <valdircpd@users.noreply.github.com>
2025-12-30 13:05:29 -0300

    Update firebase-config.js

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,1 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA3e7KB1R1SbYb_HKGm52DyqAEeAN9Qpdw",
+  authDomain: "sistema-fluxo-caixa-espetinho.firebaseapp.com",
+  projectId: "sistema-fluxo-caixa-espetinho",
+  storageBucket: "sistema-fluxo-caixa-espetinho.firebasestorage.app",
+  messagingSenderId: "628667049442",
+  appId: "1:628667049442:web:125e5eb32a3274dcad05bb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
